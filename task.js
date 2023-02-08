43ee9482350957e3e867d6aad28c0147cc021570
Ann Sarviro <annka_nuha@ai.ua>
2023-02-08 20:11:32 +0200

    task 08.02

--- task.js
+++ task.js
@@ -64,3 +64,27 @@ function order(words) {
 function repeatStr(n, s) {
   return s.repeat(n)
 }
+
+// -----------------------------//
+// A pangram is a sentence that contains every single letter of the alphabet at least once. 
+// For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+// Given a string, detect whether or not it is a pangram. 
+// Return True if it is, False if not. Ignore numbers and punctuation.
+
+function isPangram(string){
+  let alfabet = "abcdefghijklmnopqrstuvwxyz"
+  let arr = string.toLowerCase().split('')
+  let filteredArr = arr.filter((val, index, array)=> array.indexOf(val)===index)
+  let res= filteredArr.reduce((acc, val, i)=> {
+    alfabet.includes(val) ? acc = acc + 1 : acc=acc
+    return acc},0)
+  
+  return res === alfabet.length ? true : false
+}
+
+// ore
+function isPangrams(string){
+  return 'abcdefghijklmnopqrstuvwxyz'
+    .split('')
+    .every((x) => string.toLowerCase().includes(x));
+}
