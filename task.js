792ef84d6e79facbb3146c938eb7283fbbfbb6c0
Ann Sarviro <annka_nuha@ai.ua>
2023-02-13 00:36:17 +0200

    task 12.02

--- task.js
+++ task.js
@@ -126,3 +126,20 @@ function accum(s) {
   return res.join('')
 	// your code
 }
+// -----------------------------------//
+// Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+// You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+// The array will always contain letters in only one case.
+// Example:
+// ['a','b','c','d','f'] -> 'e'
+// ['O','Q','R','S'] -> 'P'
+
+function findMissingLetter(array)
+{
+  const alfabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  for(let i=0; i<array.length; i+=1){
+    let index = alfabet.indexOf(array[i])
+        if(array[i+1] !== alfabet[index+1]) {
+        return alfabet[index+1]} }
+     
+}
