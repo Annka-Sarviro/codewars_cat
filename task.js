18875770c04cccdc023e53b1f61aab7f09964da2
Ann Sarviro <annka_nuha@ai.ua>
2023-02-13 13:08:21 +0200

    task 13.02

--- task.js
+++ task.js
@@ -143,3 +143,18 @@ function findMissingLetter(array)
         return alfabet[index+1]} }
      
 }
+// ------------------------------------//
+// Given an array of integers, find the one that appears an odd number of times.
+// There will always be only one integer that appears an odd number of times.
+// Examples
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+
+function findOdd(A) {
+  let res
+  A.map((val, i, arr)=> {
+   let b = A.filter((a)=> a===val)
+   if (b.length%2 !== 0 ) {return res = val}})
+  return res;
+}
