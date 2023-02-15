f07ad7de9181799c0eee5b9e8756428180296193
Ann Sarviro <annka_nuha@ai.ua>
2023-02-16 00:18:03 +0200

    task_16.02

--- task.js
+++ task.js
@@ -183,3 +183,15 @@ function persistence(num) {
 
   return count
 }
+
+// ========================//
+// Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number.
+// You can guarantee that input is non-negative.
+// Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+
+var countBits = function (n) {
+  let number = n.toString(2).split('')
+  return number.reduce((acc, val, i) => {
+    return acc + parseInt(val, 10)
+  }, 0)
+}
